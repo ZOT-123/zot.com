b803967af85e287a642144d3b224e6b52cbd3fc7
ZOT-123 <hjl1357275420@hotmail.com>
2025-05-18 12:50:53 +0800

    Add files via upload

---
+++ css/flies.js
@@ -0,0 +1,22 @@
+window.flies = [
+  {
+    "title": "Ubuntu 24.04 LTS",
+    "desc": "流行的 Linux 发行版，适合服务器与桌面",
+    "url": "www.baidu.com"
+  },
+  {
+    "title": "Python 3.12.3",
+    "desc": "主流编程语言，适用于开发与数据分析",
+    "url": "#"
+  },
+  {
+    "title": "Node.js 20.x",
+    "desc": "高性能 JavaScript 运行环境",
+    "url": "#"
+  },
+  {
+    "title": "Anaconda 2024",
+    "desc": "数据科学与机器学习集成环境",
+    "url": "#"
+  }
+];

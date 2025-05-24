46b82fda0b7cfa305e572bbced5da1444349bb57
ZOT-123 <hjl1357275420@hotmail.com>
2025-05-25 00:44:36 +0800

    Add files via upload

---
+++ css/mirrorlinks.js
@@ -0,0 +1,18 @@
+window.flies = {
+  windows: [
+     {
+      "title": "null",
+      "desc": "null",
+      "url": "null",
+      "target": "_blank"
+    }
+  ],
+  linux: [
+     {
+      "title": "null",
+      "desc": "null",
+      "url": "null",
+      "target": "_blank"
+    }
+  ]
+};
